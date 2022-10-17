06f9098c128042f202967256a86979aeb3f86c89
MatheusLevy <levybessa2001@hotmail.com>
2022-10-17 18:28:49 -0300

    Atualizado TipoAnimal Controller

--- src/main/java/com/produtos/apirest/controller/TipoAnimalController.java
+++ src/main/java/com/produtos/apirest/controller/TipoAnimalController.java
@@ -1,5 +1,6 @@
 package com.produtos.apirest.controller;
 
+import com.produtos.apirest.models.DTO.TipoAnimalDTO;
 import com.produtos.apirest.models.TipoAnimal;
 import com.produtos.apirest.service.TipoAnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,20 @@ public class TipoAnimalController {
     @Autowired
     public TipoAnimalService tipoService;
 
-    //TOOD: ### ** Substituir TipoAnimal por TipoAnimalDTO **
     @PreAuthorize("hasRole('A')")
     @PostMapping("/salvar")
-    public ResponseEntity salvar(@RequestBody TipoAnimal tipo_animal){
+    public ResponseEntity salvar(@RequestBody TipoAnimalDTO dto){
         try{
-            TipoAnimal tipoSalvo = tipoService.salvar(tipo_animal);
-            return new ResponseEntity(tipoSalvo, HttpStatus.CREATED);
+            TipoAnimal tipoAnimal = TipoAnimal.builder()
+                    .nome(dto.getNome())
+                    .build();
+            TipoAnimal tipoSalvo = tipoService.salvar(tipoAnimal);
+
+            TipoAnimalDTO dtoRetorno = TipoAnimalDTO.builder()
+                    .id(tipoSalvo.getTipoAnimalId())
+                    .nome(tipoSalvo.getNome())
+                    .build();
+            return new ResponseEntity(dtoRetorno, HttpStatus.CREATED);
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -38,25 +46,36 @@ public class TipoAnimalController {
         }
     }
 
-    //TOOD: ### ** Substituir TipoAnimal por TipoAnimalDTO **
     @PreAuthorize("hasRole('A')")
     @DeleteMapping("/remover/feedback")
-    public ResponseEntity removerComFeedback(@RequestBody TipoAnimal tipo){
+    public ResponseEntity removerComFeedback(Long id){
         try{
-            TipoAnimal tipoRemovido = tipoService.removerFeedback(tipo.getTipoAnimalId());
-            return ResponseEntity.ok(tipoRemovido);
+            TipoAnimal tipoRemovido = tipoService.removerFeedback(id);
+            TipoAnimalDTO dtoRetorno = TipoAnimalDTO.builder()
+                    .id(tipoRemovido.getTipoAnimalId())
+                    .nome(tipoRemovido.getNome())
+                    .build();
+            return ResponseEntity.ok(dtoRetorno);
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
-    //TOOD: ### ** Substituir TipoAnimal por TipoAnimalDTO **
     @PreAuthorize("hasRole('A')")
     @PutMapping("/atualizar")
-    public ResponseEntity atualizar(@RequestBody TipoAnimal tipo_animal){
+    public ResponseEntity atualizar(@RequestBody TipoAnimalDTO dto){
         try{
-            TipoAnimal atualizado = tipoService.atualizar(tipo_animal);
-            return ResponseEntity.ok(atualizado);
+            TipoAnimal tipoAnimal = TipoAnimal.builder()
+                    .tipoAnimalId(dto.getId())
+                    .nome(dto.getNome())
+                    .build();
+            TipoAnimal atualizado = tipoService.atualizar(tipoAnimal);
+
+            TipoAnimalDTO dtoRetorno = TipoAnimalDTO.builder()
+                    .id(atualizado.getTipoAnimalId())
+                    .nome(atualizado.getNome())
+                    .build();
+            return ResponseEntity.ok(dtoRetorno);
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -67,10 +86,14 @@ public class TipoAnimalController {
     public ResponseEntity buscar(@PathVariable(value = "id", required = true) Long id){
         try{
             TipoAnimal tipoBuscado = tipoService.buscarTipoAnimalPorId(id);
-            return ResponseEntity.ok(tipoBuscado);
+
+            TipoAnimalDTO dtoRetorno = TipoAnimalDTO.builder()
+                    .id(tipoBuscado.getTipoAnimalId())
+                    .nome(tipoBuscado.getNome())
+                    .build();
+            return ResponseEntity.ok(dtoRetorno);
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
 }
